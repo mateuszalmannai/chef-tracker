2c769ff1c00edf74350a02ed896e83920bf1eba0
mateusz <mateuszalmannai@googlemail.com>
2017-05-29 18:21:01 +0200

    add array of objects in controller and access properties from template

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  chefs: ['Gordon Ramsay', 'Anthony Bourdain', 'Rachael Ray', 'Jamie Oliver', 'Guy Fieri']
+  chefs: [
+    {name: 'Gordon Ramsay', cookingToday: true},
+    {name: 'Anthony Bourdain', cookingToday: false},
+    {name: 'Rachael Ray', cookingToday: true},
+    {name: 'Jamie Oliver', cookingToday: false},
+    {name: 'Guy Fieri', cookingToday: true}]
 });
