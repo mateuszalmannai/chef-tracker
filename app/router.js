10afec549377842fbbd3500a2a54a5f2bbf14796
mateusz <mateuszalmannai@googlemail.com>
2017-05-31 21:50:37 +0200

    let the user change name and description, save the info and transition to show page

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('chefs', function(){
     this.route('chef', {path: ':chef_id'});
+    this.route('chef.edit', {path: ':chef_id/edit'});
   });
   this.route('about');
   this.route('contact');
