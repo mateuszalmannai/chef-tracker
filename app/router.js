f1dfafa4abd1e0feb1c3953357fc92739fea82a8
mateusz <mateuszalmannai@googlemail.com>
2017-05-31 14:25:36 +0200

    add nested routing for show page + styling

--- app/router.js
+++ app/router.js
@@ -7,7 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('chefs');
+  this.route('chefs', function(){
+    this.route('chef', {path: ':chef_id'});
+  });
   this.route('about');
   this.route('contact');
 });
