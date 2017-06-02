ac24ef80d72655783263985a72e123f4d59f6186
mateusz <mateuszalmannai@googlemail.com>
2017-06-02 20:47:41 +0200

    create show page

--- app/router.js
+++ app/router.js
@@ -1,19 +1,21 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from "ember";
+import config from "./config/environment";
 
 const Router = Ember.Router.extend({
   location: config.locationType,
   rootURL: config.rootURL
 });
 
-Router.map(function() {
-  this.route('chefs', function(){
+Router.map(function () {
+  this.route('chefs', function () {
     this.route('chef', {path: ':chef_id'});
     this.route('chef.edit', {path: ':chef_id/edit'});
   });
   this.route('about');
   this.route('contact');
-  this.route('restaurants');
+  this.route('restaurants', function () {
+    this.route('restaurant', {path: ':restaurant_id'});
+  });
 });
 
 export default Router;
