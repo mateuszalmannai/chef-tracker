2f5be657744afede37fbefb4f7a12d1015d6ae13
mateusz <mateuszalmannai@googlemail.com>
2017-06-02 20:55:19 +0200

    create edit page and add styling

--- app/router.js
+++ app/router.js
@@ -15,6 +15,7 @@ Router.map(function () {
   this.route('contact');
   this.route('restaurants', function () {
     this.route('restaurant', {path: ':restaurant_id'});
+    this.route('restaurant.edit', {path: ':restaurant_id/edit'});
   });
 });
 
